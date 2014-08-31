ed5629dd597f31561ee4204e321e435bad098cc1
John Schmerge <john.schmerge@gmail.com>
2014-08-30 21:44:02 -0400

    Added ghetto installer and fixed YCM's flag file's name

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+rootLocation=$(dirname $0)
+rootLocation=$(realpath $rootLocation)
+
+echo "Base location = $rootLocation"
+
+for file in ${rootLocation}/dotfiles/*
+do
+	echo "Processing ${file}..."
+	name=$(basename $file)
+	destination="${HOME}/.${name}"
+	skip=1
+
+#	[ -f ${destination} ] && echo a
+#	[ -L ${destination} ] && echo b
+#	[ -L ${destination} ] && echo c
+#	[ -e ${destination} ] && echo d
+	if [ -f ${destination} -a ! -L ${destination} ]
+	then
+		echo "  Backing up current ${destination} to ${destination}.bak"
+		[ -e "${destination}.bak" ] && rm -vf "${destination}.bak"
+		mv -v ${destination} ${destination}.bak
+		skip=0
+	elif [ -L ${destination} ]
+	then
+		echo "  Removing symlink ${destination}"
+		rm -vf ${destination}
+		skip=0
+	elif [ -e ${destination} ]
+	then
+		echo "  Skipping install over the following destination:"
+		ls -l ${destination}
+	else
+		echo "  Creating new symlink:"
+		skip=0
+	fi
+
+	if [ "$skip" != 1 ]
+	then
+		ln -sv ${file} ${destination}
+	fi
+
+done
